c605e2d945d5ccbc654418b67c1e3ad2a21c492c
a0323i1thaison <sontds170551@fpt.edu.vn>
2023-08-07 18:10:29 +0700

    sum

---
+++ src/ss9_DSA/bai_tap/TestLinkList.java
@@ -0,0 +1,19 @@
+package ss9_DSA.bai_tap;
+import java.util.LinkedList;
+public class TestLinkList {
+    public static void main(String[] args) {
+        MyLinkList<Integer> integerMyLinkList = new MyLinkList<>();
+
+        LinkedList test = new LinkedList();
+        integerMyLinkList.addFirst(1);
+        integerMyLinkList.add(1, 2);
+        integerMyLinkList.add(2, 3);
+        integerMyLinkList.add(3, 4);
+        integerMyLinkList.addLast(5);
+
+        System.out.println("Display linked list: ");
+        for (int i = 0; i < integerMyLinkList.numNodes; i++) {
+            System.out.println("Node " + (i + 1) + ": " + integerMyLinkList.get(i));
+        }
+    }
+}
